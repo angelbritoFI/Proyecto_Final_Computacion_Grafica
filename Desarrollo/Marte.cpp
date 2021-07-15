fb0df38fc8bdb841501cdb524b1f57a90ff36cc3
AgustinHT <agustinht.fi@gmail.com>
2021-07-15 12:24:42 -0500

    Aparición del Sol o la Luna dependiendo si es de día o de noche

--- Desarrollo/Marte.cpp
+++ Desarrollo/Marte.cpp
@@ -440,6 +440,13 @@ int main() {
 			tmp_skybox += tiempo_offset * deltaTime;
 			skybox.DrawSkybox(camera.calculateViewMatrix(), projection);
 			shaderList[0].UseShader();
+
+			glm::mat4 model(1.0);
+			model = glm::mat4(1.0);
+			model = glm::translate(model, glm::vec3(-200.0f, 100.0f, 20.0f));
+			model = glm::scale(model, glm::vec3(30.0f, 30.0f, 30.0f));
+			glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+			Sol_M.RenderModel();
 			// Si el tiempo es el último tiempo para el skybox actual, entonces se debe cambiar el tipo de skybox
 			if (tmp_skybox >= 14.99f)
 				skyboxNoche = true;
@@ -450,6 +457,13 @@ int main() {
 			tmp_skybox -= tiempo_offset * deltaTime;
 			skyboxNight.DrawSkybox(camera.calculateViewMatrix(), projection);
 			shaderList[0].UseShader();
+
+			glm::mat4 model(1.0);
+			model = glm::mat4(1.0);
+			model = glm::translate(model, glm::vec3(-200.0f, 100.0f, 20.0f));
+			model = glm::scale(model, glm::vec3(15.0f, 15.0f, 15.0f));
+			glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+			Luna_M.RenderModel();
 
 			// Si el tiempo es el último tiempo para el skybox actual, entonces se debe cambiar el tipo de skybox
 			if (tmp_skybox <= 0.01f)
@@ -550,6 +564,7 @@ int main() {
 		model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(15.0f, 50.0f, 20.0f));
 		model = glm::scale(model, glm::vec3(5.0f, 5.0f, 5.0f));
+		model = glm::rotate(model, -90 * toRadians, glm::vec3(1.0f, 0.0f, 0.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		InterceptorJedi_M.RenderModel();
 
@@ -597,17 +612,17 @@ int main() {
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		DeathStar_M.RenderModel();
 
-		model = glm::mat4(1.0);
+		/*model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(200.0f, 100.0f, 20.0f));
 		model = glm::scale(model, glm::vec3(15.0f, 15.0f, 15.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		Luna_M.RenderModel();
+		Luna_M.RenderModel();*/
 
-		model = glm::mat4(1.0);
+		/*model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(-200.0f, 100.0f, 20.0f));
 		model = glm::scale(model, glm::vec3(30.0f, 30.0f, 30.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		Sol_M.RenderModel();
+		Sol_M.RenderModel();*/
 		
 		glUseProgram(0);
 
